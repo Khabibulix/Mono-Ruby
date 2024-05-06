75456d8a6643d336192a1ef86c24bb0d17776640
Khab <unitetempete@gmail.com>
2024-05-06 16:14:09 +0200

    Working function for default rotation value

--- CaesarCipher/main.rb
+++ CaesarCipher/main.rb
@@ -1,13 +1,18 @@
 
 def caesar_cipher(str_to_encrypt, number_of_rotations=2)
     alphabet = "abcdefghijklmnopqrstuvwxyz"
+    encrypted_str = ""
     str_to_encrypt.each_char do |char|
         if alphabet.include?(char.downcase)
-            puts char
+            current_index_of_letter = alphabet.index(char.downcase)
+            # attention valeur trop faible ici
+            new_index_of_letter = current_index_of_letter - number_of_rotations
+            encrypted_str += alphabet[new_index_of_letter]
         else
-            puts char
+            encrypted_str += char
         end
     end
+    puts encrypted_str
 end
 
-caesar_cipher("Test!")
+caesar_cipher("test!")
