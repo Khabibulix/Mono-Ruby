1368a26f4ccc6984df084a9ac2e695126af2ca18
Khab <unitetempete@gmail.com>
2024-05-07 16:10:40 +0200

    BUBBLESORT -- Creating necessary variables to begin iteration

---
+++ BubbleSort/main.rb
@@ -0,0 +1,10 @@
+exercise_array = [4,3,78,2,0,2]
+
+def bubble_sort(array_to_sort)
+    index_of_actual_element = 0
+    actual_element = array_to_sort[index_of_actual_element]
+    index_of_next_element = 1
+    next_element = array_to_sort[index_of_next_element]
+    number_of_swaps = 0
+    
+end
