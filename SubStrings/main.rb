2850676b70f11f6bf55289925da7af5cad26f1fc
Khab <unitetempete@gmail.com>
2024-05-07 15:44:47 +0200

    SUBSTRINGS -- Slicing is correct, now removing empty values in hash

--- SubStrings/main.rb
+++ SubStrings/main.rb
@@ -1,11 +1,20 @@
-def substrings(str_to_check="gogo", data_structure_to_seek)
+def substrings(str_to_check, data_structure_to_seek)
     result = {}
     result[str_to_check] = data_structure_to_seek.count(str_to_check)
+    
+    for sliced_str in 2...str_to_check.length
+        result[str_to_check.slice(0,sliced_str)] = data_structure_to_seek.count(str_to_check.slice(0,sliced_str))
+    end
+
+    for sliced_str in -str_to_check.length + 1...0
+        result[str_to_check.slice(-sliced_str..)] = data_structure_to_seek.count(str_to_check.slice(-sliced_str..))
+    end
+    
     puts result
 end
 
 
 
-dictionary = ["below","down","go","gogo","going","horn","how", "gaga", "gogo"]
+dictionary = ["below","gog","down","go","gogo","low","going","horn","how", "gaga", "gogo"]
 
-substrings(dictionary)
+substrings("below", dictionary)
