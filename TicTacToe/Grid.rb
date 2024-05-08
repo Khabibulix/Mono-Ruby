b45ebd870ed01ffbc059286eefce2ddbbf1b7315
Khab <unitetempete@gmail.com>
2024-05-08 21:49:00 +0200

    TICTACTOE -- Adding basic functionality to create the grid, hardcoded for now

--- TicTacToe/Grid.rb
+++ TicTacToe/Grid.rb
@@ -1,17 +1,31 @@
+require './Case.rb'
+
 class Grid < Case
     attr_reader :width    
     def initialize(width)
         @width = width
     end
 
-    def create_grid(self.width)
+    def create_grid()
+        for row in 0...self.width
+            for col in 0...self.width
+                print(".  ")
+            end
+            print("\n")
+        end
+
     end
 
-    def is_grid_full(self)
-    end
+    # def is_grid_full(self)
+    # end
 
-    def empty_the_grid(self)
-    end
+    # def empty_the_grid(self)
+    # end
 
-    def get_empty_cells(self)
-    end
+    # def get_empty_cells(self)
+    # end
+
+end
+
+grid = Grid.new(3)
+grid.create_grid
