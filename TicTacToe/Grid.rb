324176a0b6dc77501acd2b0d0d2ff5fffb31b4d4
Khab <unitetempete@gmail.com>
2024-05-08 21:32:30 +0200

    TICTACTOE -- Adding basic skeleton for class Grid with parameters and future methods

---
+++ TicTacToe/Grid.rb
@@ -0,0 +1,17 @@
+class Grid < Case
+    attr_reader :width    
+    def initialize(width)
+        @width = width
+    end
+
+    def create_grid(self.width)
+    end
+
+    def is_grid_full(self)
+    end
+
+    def empty_the_grid(self)
+    end
+
+    def get_empty_cells(self)
+    end
