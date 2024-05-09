acb9bc5ea85f64b7d2d0ac9b6ef13614c265e608
Khab <unitetempete@gmail.com>
2024-05-09 14:55:47 +0200

    TICTACTOE -- Grabbing the player_symbol is OK, now adding computer_symbol

--- TicTacToe/Game.rb
+++ TicTacToe/Game.rb
@@ -1,11 +1,38 @@
 require './Grid.rb'
 
 class Game
-    attr_reader :grid
+    attr_reader :grid, :player_symbol
     def initialize(grid)
         @grid = grid
+        @player_symbol = ""
     end
+
+    def get_symbol_for_player()
+        puts "Choose your symbol!"
+        player_symbol = gets.chomp()
+        if player_symbol.length != 1
+            puts "Too long, we're gonna take the first character, sorry..."
+            @player_symbol += player_symbol[0]
+        else
+            @player_symbol += player_symbol
+        end
+
+    end
+
+    # def check_for_end_of_game()
+    # end
+
+    # def check_for_victory()
+    # end
+
+    # def get_player_input()
+    # end
+
+    # def get_computer_input()
+    # end
+
 end
 
 game = Game.new(Grid.new(3))
-game.grid.display_grid
+game.get_symbol_for_player
+p game.player_symbol
