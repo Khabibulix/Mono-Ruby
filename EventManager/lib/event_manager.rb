95076a456eca6d596188f8ea3c66019b87510db7
Khab <unitetempete@gmail.com>
2024-05-13 00:54:07 +0200

    EVENT_MANAGER -- Beginning operations about reading file

--- EventManager/lib/event_manager.rb
+++ EventManager/lib/event_manager.rb
@@ -1,1 +1,8 @@
-puts "Yup"
+content = File.read("event_attendees.csv") if File.exist? "event_attendees.csv"
+lines = File.readlines("event_attendees.csv")
+lines.each do |line|
+    columns = line.split(",")
+    next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
+    name = columns[2]
+    puts name
+end
