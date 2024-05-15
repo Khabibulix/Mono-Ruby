64d61403206087a46593208e9b27836a92113087
Khab <unitetempete@gmail.com>
2024-05-15 17:29:18 +0200

    HANGMAN -- Adding method to filter words that are too short or too long and stocking them in an array

--- Hangman/lib/game.rb
+++ Hangman/lib/game.rb
@@ -1,3 +1,15 @@
-wordlist = File.open(Dir.pwd + "/wordlist.txt")
+def stock_words_of_correct_size
+    wordlist = File.new(Dir.pwd + "/wordlist.txt")
 
-p wordlist
+    if File.exist? wordlist
+        lines = wordlist.readlines
+        playable_words = []
+        lines.each do |line|
+            playable_words.push(line.chomp)
+        end
+        playable_words.select {|item| item.length < 12 && item.length > 5}
+    end
+    playable_words
+end
+
+p stock_words_of_correct_size
