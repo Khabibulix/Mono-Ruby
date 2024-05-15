395d145311c9bb59413af6b53649f44e145c5857
Khab <unitetempete@gmail.com>
2024-05-15 22:23:04 +0200

    HANGMAN -- Adding method to display the hidden word, now working on guessing a letter

--- Hangman/lib/game.rb
+++ Hangman/lib/game.rb
@@ -1,16 +1,38 @@
-def stock_words_of_correct_size
-    wordlist = File.new("#{Dir.pwd}/data/wordlist.txt")
-
-    if File.exist? wordlist
-        lines = wordlist.readlines
-        playable_words = []
-        lines.each do |line|
-            playable_words.push(line.chomp)
-        end
-        playable_words.select! {|item| item.length < 12 && item.length > 5}
+class Game
+    attr_reader :word, :incorrect_guess, :choosed_letters        
+    def initialize
+        @word = choose_word
+        @incorrect_guesses = 5
+        @choosed_letters = []
     end
-    playable_words
+
+    def stock_words_of_correct_size
+        wordlist = File.new("#{Dir.pwd}/data/wordlist.txt")
+
+        if File.exist? wordlist
+            lines = wordlist.readlines
+            playable_words = []
+            lines.each do |line|
+                playable_words.push(line.chomp)
+            end
+            playable_words.select! {|item| item.length < 12 && item.length > 5}
+        end
+        playable_words
+    end
+
+    def choose_word
+        wordlist = stock_words_of_correct_size
+        wordlist.sample
+    end
+
+    def display_hidden_word
+        word_hidden = ""
+        (0...@word.length).each {word_hidden += "_"}
+        word_hidden
+    end
 end
 
-p stock_words_of_correct_size
 
+
+game = Game.new()
+p game.display_hidden_word
