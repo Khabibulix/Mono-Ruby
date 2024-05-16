1c686c600ecfcf1845455b47b4ba9cf65e678a67
Khab <unitetempete@gmail.com>
2024-05-16 15:55:20 +0200

    HANGMAN -- Adding battery of tests for all methods in Game class

--- Hangman/tests/tests_game.rb
+++ Hangman/tests/tests_game.rb
@@ -6,7 +6,36 @@ class Test < Minitest::Test
         @game = Game.new
     end
 
-    def test_stock_words
+    def test_stock_words_return
         assert_instance_of(Array, @game.stock_words_of_correct_size)
     end
+
+    def test_choose_word
+        assert_includes(@game.stock_words_of_correct_size, @game.choose_word)
+    end
+
+    def test_display_hidden_word
+        assert_equal(@game.display_hidden_word.length, @game.word.length)
+    end
+
+    def test_true_is_input_in_word_for_e
+        assert(@game.is_input_in_word?("esperanza", "e"))
+    end
+
+    def test_true_is_input_in_word_for_a
+        assert(@game.is_input_in_word?("esperanza", "a"))
+    end
+
+    def test_false_is_input_in_word?
+        assert(!@game.is_input_in_word?("asparanza", "e"))
+    end
+
+    def test_get_all_index_for_letter_e
+        assert_equal([0, 3], @game.get_all_index_for_letter("esperanza", "e"))
+    end
+
+    def test_get_all_index_for_letter_a
+        assert_equal([5,8], @game.get_all_index_for_letter("esperanza", "a"))
+    end
+
 end
