e7c07971e098cbb28b1a4955bf4e259dbf5bf8ee
Khab <unitetempete@gmail.com>
2024-05-21 15:39:53 +0200

    LINKEDLIST -- #prepend method to add a node at the head of the linked list is now complete

--- LinkedList/linked_list.rb
+++ LinkedList/linked_list.rb
@@ -3,8 +3,25 @@ require "#{Dir.pwd}/node.rb"
 
 class LinkedList
 
-    def initialize
-        @head = nil
+    def initialize(head)
+        @head = head
+    end
+
+    def head
+        @head
+    end
+
+    def prepend(value)
+        current_node = Node.new(value)        
+        if is_empty?
+            @head = current_node
+            current_node.next_node = nil
+        else
+            current_node.next_node = @head
+            @head = current_node
+        end
+
+
     end
 
 
@@ -14,5 +31,9 @@ class LinkedList
 end
 
 
-ll = LinkedList.new()
-p ll.is_empty?
+node = Node.new("Value")
+ll = LinkedList.new(node)
+ll.prepend("B")
+p ll.head.to_s #Should be "B"
+
+
