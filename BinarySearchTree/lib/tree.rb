c0550bbfcc5b2f467e7f7fa3a48ff5356bd975f9
Khab <unitetempete@gmail.com>
2024-05-27 17:46:51 +0200

    BINARYSEARCHTREE -- Refactorisation to permit data visualisation, all is OK for now

--- BinarySearchTree/lib/tree.rb
+++ BinarySearchTree/lib/tree.rb
@@ -1,17 +1,28 @@
 require_relative 'node.rb'
 
 class Tree
-    attr_accessor :array
+    attr_accessor :data, :root
     def initialize(array)
-        @array = array.sort.uniq!
-        @root = Node.new(@array[@array.length / 2])
+        @data = array.sort.uniq!
+        @root = build_tree(data)
     end
 
-    def build_tree(arr=@array) 
-        return nil if arr.empty?
-        mid = arr.length / 2
-        Node.new(arr[mid], build_tree(arr[0...mid]), build_tree(arr[mid + 1..]))
-        arr
+    def pretty_print(node = root, prefix = '', is_left = true)
+        pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+        puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+        pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+    end
+
+    def build_tree(array) 
+        return nil if array.empty?
+    
+        middle = array.length / 2
+        root_node = Node.new(array[middle])
+    
+        root_node.left = build_tree(array[0...middle])
+        root_node.right = build_tree(array[(middle + 1)..-1])
+    
+        root_node 
     end
 
     def insert_node(root=@root, value)
--- BinarySearchTree/main.rb
+++ BinarySearchTree/main.rb
@@ -2,9 +2,9 @@ require_relative 'lib/tree.rb'
 
 
 t = Tree.new([1, 3, 4, 4, 5, 7, 7, 8, 9, 9, 23, 67, 324, 6345])
-t.build_tree
 t.insert_node(25)
 t.pretty_print
+
 
 
 
