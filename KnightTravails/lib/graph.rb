76d42de146e75a5b720a99908c93bf3a6c282fd4
Khab <unitetempete@gmail.com>
2024-05-29 15:51:19 +0200

    KNIGHTTRAVAILS -- Adding edges is now complete and working. Adding predicate method to check if there is an edge between two nodes

--- KnightTravails/lib/graph.rb
+++ KnightTravails/lib/graph.rb
@@ -3,11 +3,23 @@ class Graph
         @width = width
         @matrix = Array.new(width) {Array.new(width, 0)}
     end
-
+    
     def print_matrix
         @matrix.each {|row| print "#{row}\n"}
     end
+
+    def add_edge(node1, node2)
+        @matrix[node1][node2] = 1
+        @matrix[node2][node1] = 1
+    end
+
+    def has_edge?(node1, node2)
+        @matrix[node1][node2] == 1
+    end
+
 end
 
 g = Graph.new(5)
+g.add_edge(0,2)
 g.print_matrix
+p g.has_edge?(0,2)
