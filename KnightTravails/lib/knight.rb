13df4482e8fcb7dc7529bde633674fdd32f3ae3b
Khab <unitetempete@gmail.com>
2024-05-30 16:16:33 +0200

    KNIGHTTRAVAILS -- #move_valid checks is the move is ok, behaviour is correct for now

--- KnightTravails/lib/knight.rb
+++ KnightTravails/lib/knight.rb
@@ -1,3 +1,14 @@
+MOVE_SET = [
+    [-2, -1],
+    [-1, -2],
+    [1, -2],
+    [-2, 1],
+    [-1, 2],
+    [1, 2],
+    [2, -1],
+    [2, 1]
+].freeze
+
 class Knight
     attr_accessor :symbol, :x_pos, :y_pos
     def initialize(x_pos = 4, y_pos = 4)
@@ -6,7 +17,23 @@ class Knight
         @y_pos = y_pos
     end
 
-    def move
+    #move_valid?([0,1], [0,2])
+    def move_valid?(initial_pos, next_pos)
+        
+        MOVE_SET.each do |move|
+            x_pos_valid = initial_pos[0] + move[0] == next_pos[0] && next_pos[0].between?(0,8)
+            y_pos_valid = initial_pos[1] + move[1] == next_pos[1] && next_pos[1].between?(0,8)
+            if x_pos_valid && y_pos_valid
+                return true
+            end
+        end
+        false
+    end
+
+    def get_moves
         p "Nothing here for now"
     end
 end
+
+knight = Knight.new(4,4)
+p knight.move_valid?([4,4],[5,2])
