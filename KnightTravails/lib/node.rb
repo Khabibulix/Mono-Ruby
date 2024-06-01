38cfa198a04f4ab2ced0bdfad1720678cea2a30d
Khab <unitetempete@gmail.com>
2024-06-01 16:26:08 +0200

    KNIGHTTRAVAILS -- Adding #get_path to print correctly the parents of the node in argument in the node class.

--- KnightTravails/lib/node.rb
+++ KnightTravails/lib/node.rb
@@ -1,7 +1,16 @@
 class Node 
-    attr_accessor :childs, :parent
-    def initialize(childs, parent)
-        @childs = childs
+    attr_accessor :value, :parent
+    def initialize(value=nil, parent=nil)
+        @value = value
         @parent = parent
     end
+
+    def get_path(node, path_array=[node.value])
+        path_array.push(@parent)
+        return path_array if @parent.class == Array
+        
+        get_path(@parent, path_array)
+        
+    end
+
 end
