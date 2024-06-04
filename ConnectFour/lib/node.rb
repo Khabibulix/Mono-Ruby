2e20780db50517881dc7a327fa3c7205a3689edc
Khab <unitetempete@gmail.com>
2024-06-04 22:01:47 +0200

    CONNECTFOUR -- Nodes neighbors are correctly returned and tests are covering all of Node class

--- ConnectFour/lib/node.rb
+++ ConnectFour/lib/node.rb
@@ -1,7 +1,31 @@
+NEIGHBORS_CALC = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1]
+].freeze
+
 class Node
-    attr_accessor :value
-    def initialize( value = ".")
+    attr_accessor :value, :position, :neighbors
+    def initialize( value = ".", position)
         @value = value
+        @position = position
+        @neighbors = get_neighbors
     end 
+
+    def get_neighbors
+        neighbors = []
+        NEIGHBORS_CALC.each do |pos|
+            if (position[0] + pos[0]).between?(0,8) && (position[1] + pos[1]).between?(0,8)
+                neighbors.push([position[0] + pos[0], position[1] + pos[1]])
+            end
+        end
+        neighbors
+    end
+
 
 end
--- ConnectFour/spec/node_spec.rb
+++ ConnectFour/spec/node_spec.rb
@@ -1,9 +1,21 @@
 require_relative '../lib/node'
 
 describe do
-    before(:all) {@node = Node.new}
+    before(:all) do
+        @node = Node.new([4,4])
+        @node_border = Node.new([0,0])
+    end
 
     specify "Node default value should be a dot" do
         expect(@node.value).to eq "."
+    end
+
+    specify "Should return an array of neighbors for the center node" do
+        result = [[3,3], [3,4], [3,5], [4,3], [4,5], [5,3], [5,4], [5,5]]
+        expect(@node.neighbors).to eq result
+    end
+
+    specify "Should return an array of neighbors excluding falsy one" do
+        expect(@node_border.neighbors).to eq [[0,1], [1,0], [1,1]]
     end
 end
