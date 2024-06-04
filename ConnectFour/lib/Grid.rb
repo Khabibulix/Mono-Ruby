6b97ed598bedb5149c32e067974adcbbac1bf981
Khab <unitetempete@gmail.com>
2024-06-04 14:47:23 +0200

    CONNECTFOUR -- Adding #create_grid and its green test, Grid now has a structure on its own

--- ConnectFour/lib/Grid.rb
+++ ConnectFour/lib/Grid.rb
@@ -1,6 +1,13 @@
 class Grid
-    attr_accessor :total_size
+    attr_accessor :total_size, :grid, :columns, :rows
     def initialize(columns = 7, rows = 6)
         @total_size = columns * rows
+        @columns = columns
+        @rows = rows
+        @grid = create_grid
+    end
+
+    def create_grid
+        Array.new(rows) {Array.new(columns, ".")}
     end
 end
--- ConnectFour/spec/grid_spec.rb
+++ ConnectFour/spec/grid_spec.rb
@@ -1,10 +1,15 @@
 require_relative '../lib/grid'
 
-describe "A Grid should have a constant length" do
-    context "Should have a 7x6 where 7 is the width" do
-        it "Total cells should be 42" do
-            grid = Grid.new
-            expect(grid.total_size).to eq 42
-        end
+describe do
+
+    before(:all) {@grid = Grid.new}
+
+    specify "Total cells should be 42 or 6*7" do
+        expect(@grid.total_size).to eq 42
     end
+
+    specify "Grid should be filled with dots at init" do
+        expect(@grid.grid[0][0]).to eq "."
+    end
+
 end
