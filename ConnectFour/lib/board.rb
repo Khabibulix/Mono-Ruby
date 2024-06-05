af8de386ce91a63479eaaf7bf85b31c89237e546
Khab <unitetempete@gmail.com>
2024-06-05 16:25:05 +0200

    CONNECTFOUR -- Work on nodes is correct, now each cell is a Node with its neighbors and its value

--- ConnectFour/lib/board.rb
+++ ConnectFour/lib/board.rb
@@ -1,3 +1,5 @@
+require_relative 'node'
+
 class Board
     attr_accessor :total_size, :grid, :columns, :rows
     def initialize(columns = 7, rows = 6)
@@ -11,12 +13,19 @@ class Board
         Array.new(rows) {Array.new(columns, ".")}
     end
 
+    def nodify_all_cells
+        @grid.each_with_index do |row, row_index|
+            row.each_with_index do |cell, col_index|
+                @grid[row_index][col_index] = Node.new([row_index, col_index])
+            end
+        end
+        @grid
+    end
+
     def add(column, symbol)        
         return grid[column][-1] = symbol if grid[column].all?(".")
-        grid[column].to_enum.with_index.reverse_each do |cell, index|
-            if cell == "."
-                return grid[column][index] = symbol
-            end
+        grid[column].to_enum.with_index.reverse_each do |node, index|
+            return grid[column][index].value = symbol if node.value == "."
         end
     end
 end
--- ConnectFour/spec/board_spec.rb
+++ ConnectFour/spec/board_spec.rb
@@ -2,25 +2,32 @@ require_relative '../lib/board'
 
 describe do
 
-    before(:all) {@board = Board.new}
+    before(:all) do 
+        @board = Board.new
+        @board.nodify_all_cells
+    end
 
     specify "Total cells should be 42 or 6*7" do
         expect(@board.total_size).to eq 42
     end
 
     specify "Grid should be filled with dots at init" do
-        expect(@board.grid[0][0]).to eq "."
+        expect(@board.grid[0][0].value).to eq "."
     end
 
     specify "Adding to the grid should change bottom of column in parameter" do
         @board.add(0, "X")
-        expect(@board.grid[0][-1]).to eq "X"
+        expect(@board.grid[0][-1].value).to eq "X"
     end
 
     specify "Adding to the grid with pawn at the bottom should add on top" do
         @board.add(0, "X")
         @board.add(0, "X")
-        expect(@board.grid[0][-2]).to eq "X"
+        expect(@board.grid[0][-2].value).to eq "X"
+    end
+
+    specify "Grid item should be of Node Class after nodification" do
+        expect(@board.grid[0][0].class).to eq Node
     end
 
 end
