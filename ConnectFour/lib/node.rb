ae25e5d16251d17bdc12f01ce0d82029eeef3c60
Khab <unitetempete@gmail.com>
2024-06-09 09:11:20 +0200

    CONNECTFOUR -- Deleting useless code in node file, aka, neighbors

--- ConnectFour/lib/node.rb
+++ ConnectFour/lib/node.rb
@@ -6,22 +6,9 @@ DIAGONALS_NEIGHBORS_CALC = [
 ].freeze
 
 class Node
-    attr_accessor :value, :position, :neighbors
+    attr_accessor :value, :position
     def initialize( value = ".", position)
         @value = value
         @position = position
-        @neighbors = get_neighbors
     end 
-
-    def get_neighbors
-        neighbors = []
-        DIAGONALS_NEIGHBORS_CALC.each do |pos|
-            if (position[0] + pos[0]).between?(0,8) && (position[1] + pos[1]).between?(0,8)
-                neighbors.push([position[0] + pos[0], position[1] + pos[1]])
-            end
-        end
-        neighbors
-    end
-
-
 end
