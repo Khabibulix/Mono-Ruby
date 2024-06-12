70d28514dcd7282499eb5b1b362a525f1f10f525
Khab <unitetempete@gmail.com>
2024-06-12 10:41:40 +0200

    CHESS -- Cleaning code in Rook, abstracting Piece

--- Chess/lib/pieces/piece.rb
+++ Chess/lib/pieces/piece.rb
@@ -17,4 +17,29 @@ class Piece
     def inside?(pos)
         return true ? pos[0].between?(0,8) && pos[1].between?(0,8) : false
     end
+
+    def valid?(current, next_pos)
+        if inside?(next_pos)
+            return true ? current[0] == next_pos[0] || current[1] == next_pos[1] : false
+        end    
+    end
+
+    def generate_moves
+        initial_row = current_pos[0]
+        initial_col = current_pos[1]
+
+        @board.grid.each_with_index do |row, index|
+                moves.push([index, initial_col]) if index != initial_row
+                moves.push([initial_row, index]) if index != initial_col
+        end
+        moves
+    end
+
+
+    def move(next_pos)
+        if valid?(@current_pos, next_pos)
+            @current_pos = next_pos
+            @board.update_cell(next_pos, @symbol)
+        end
+    end
 end
--- Chess/lib/pieces/rook.rb
+++ Chess/lib/pieces/rook.rb
@@ -7,9 +7,11 @@ class Rook < Piece
     def initialize(color, number)
         super
         @symbol = color == "white" ? "\u265C" : "\u2656"
-        @white_pos = [[7,0],[7,7]]
-        @black_pos = [[0,0], [0,7]]
-        @initial_pos = color == "white" ? @white_pos[number - 1] : @black_pos[number - 1]
+        @possible_pos = {
+            "white" => [[7,0],[7,7]],
+            "black" => [[0,0], [0,7]]
+        }
+        @initial_pos = color == "white" ? @possible_pos["white"][number - 1] : @possible_pos["black"][number - 1]
         @current_pos = initial_pos if @current_pos.empty?
         @board = $board
         @moves = generate_moves
@@ -21,30 +23,16 @@ class Rook < Piece
     end
 
     def valid?(current, next_pos)
-        if inside?(next_pos)
-            return true ? current[0] == next_pos[0] || current[1] == next_pos[1] : false
-        end    
+        super
     end
 
 
     def generate_moves
-        initial_row = current_pos[0]
-        initial_col = current_pos[1]
-
-        @board.grid.each_with_index do |row, index|
-                moves.push([index, initial_col]) if index != initial_row
-                moves.push([initial_row, index]) if index != initial_col
-        end
-        moves
+        super
     end
 
     def move(next_pos)
-        if valid?(@current_pos, next_pos)
-            @current_pos = next_pos
-            @board.update_cell(next_pos, @symbol)
-        end
+        super
     end
-end
 
-r = Rook.new("white", 1)
-p r.moves
+end
